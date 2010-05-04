de7c9413fdff86cfad8219b62629d3cf75fee6ad
rms <yamanetoshi@gmail.com>
2010-05-04 17:29:00 +0900

    implement google map

--- src/jp/shuri/yamanetoshi/androidexercise/AndroidExercise.java
+++ src/jp/shuri/yamanetoshi/androidexercise/AndroidExercise.java
@@ -2,12 +2,16 @@ package jp.shuri.yamanetoshi.androidexercise;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class AndroidExercise extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+    	WebView webView = new WebView(this);
+    	webView.getSettings().setJavaScriptEnabled(true);
+    	webView.loadUrl("file:///android_asset/index.html");
+    	setContentView(webView);
     }
 }
