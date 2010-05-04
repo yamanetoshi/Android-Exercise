172e974076a3df81360e47015c504bc1dbab5e0e
rms <yamanetoshi@gmail.com>
2010-05-04 16:36:35 +0900

    first commit

---
+++ src/jp/shuri/yamanetoshi/androidexercise/AndroidExercise.java
@@ -0,0 +1,13 @@
+package jp.shuri.yamanetoshi.androidexercise;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidExercise extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
